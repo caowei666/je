2ba2c940d271eef7c2128fd58d370281f5812409
caowei <1029142064@qq.com>
2020-09-04 01:18:23 +0800

    springMVC整合shiro

---
+++ shirospringMVC0903/src/main/java/controller/LoginController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/gologin.html")
+    public String gologin(){
+        return "login";
+    }
+
+    @RequestMapping("/index.html")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/role.html")
+    public String role(){
+        return "role";
+    }
+
+    @RequestMapping("/menu/user.html")
+    public String user(){
+        return "menu";
+    }
+
+    @RequestMapping("/login.html")
+    public String login(String username, String password, HttpServletRequest req){
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            req.setAttribute("error","登录失败");
+            return "login";
+        }
+        return "redirect:/index.html";
+    }
+}
---
+++ shirospringMVC0903/target/shiro-springMVC-0903-1.0-SNAPSHOT/WEB-INF/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 86133
+  Date: 2020/9/3
+  Time: 22:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="login.html" method="post">
+    用户名：<input name="username">
+    密码:<input name="password">
+    <input type="submit" value="登录">
+</form>
+</body>
+</html>
