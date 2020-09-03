a19884059eaf1c35ecfd11550894fb2ae18a58e7
caowei <1029142064@qq.com>
2020-09-03 23:41:32 +0800

    servlet集成shiro

---
+++ shiro0902/src/main/java/com/test/shiro/servlet/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.test.shiro.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login.html")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            request.setAttribute("error", "登录失败");
+//            用转发才能访问到服务器内部资源
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+        response.sendRedirect("/index.html");
+    }
+}
---
+++ shiro0902/target/shiro0902-1.0-SNAPSHOT/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: 86133
+  Date: 2020/9/3
+  Time: 22:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+首页
+<shiro:hasRole name="admin">
+    <a href="/role.html">角色管理</a>
+</shiro:hasRole>
+
+<a href="/menu/user.html">菜单管理</a>
+</body>
+</html>
